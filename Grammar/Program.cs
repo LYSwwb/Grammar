d39db98e8cbb7c30f57ab79341de7fe680b10680
LYSwwb <mylocalmyhome@gmail.com>
2019-10-17 16:37:53 +0800

    添加练习代码

---
+++ Grammar/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammar
+{
+    delegate void print<T>(T t);
+    public delegate int show();
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            //{
+            //    List<Student> studentList = new List<Student>();
+            //    List<Scode> studentScodeList = new List<Scode>();
+            //    //print<Student> pr = show<Student>;
+
+            //    studentList.Add(new Student("张三01", 110, 1001, "一年级", DateTime.Parse("2019/05/05")));
+            //    studentList.Add(new Student("李三02", 112, 1001, "一年级", DateTime.Parse("2019/01/05")));
+            //    studentList.Add(new Student("张三03", 113, 1001, "一年级", DateTime.Parse("2019/03/05")));
+            //    studentList.Add(new Student("张三04", 114, 1001, "一年级", DateTime.Parse("2019/04/05")));
+            //    studentList.Add(new Student("张三05", 115, 1002, "一年级", DateTime.Parse("2019/05/05")));
+            //    studentList.Add(new Student("张三06", 116, 1005, "一年级", DateTime.Parse("2019/05/05")));
+            //    studentList.Add(new Student("张三07", 117, 1004, "一年级", DateTime.Parse("2019/05/05")));
+            //    studentList.Add(new Student("张三08", 118, 1002, "一年级", DateTime.Parse("2019/05/05")));
+            //    studentList.Add(new Student("张三09", 1159, 1003, "一年级", DateTime.Parse("2019/05/05")));
+
+            //    studentScodeList.Add(new Scode(110, 89, "语文"));
+            //    studentScodeList.Add(new Scode(112, 50, "语文"));
+            //    studentScodeList.Add(new Scode(113, 60, "语文"));
+            //    studentScodeList.Add(new Scode(114, 89, "语文"));
+            //    studentScodeList.Add(new Scode(110, 89, "数学"));
+            //    studentScodeList.Add(new Scode(118, 89, "语文"));
+            //    studentScodeList.Add(new Scode(119, 89, "语文"));
+
+
+            //    var student1001 = studentList.Where(s => s.ClassCode == 1001);
+            //    var listOrderBy = studentList.Where(s => s.ClassCode == 1001).OrderBy(t => t.BirthDay);
+            //    var listStartWith = studentList.Where(s => (s.ClassCode == 1001) && (s.StudentName.StartsWith("李"))).OrderByDescending(t => t.BirthDay);
+            //    var listScore = studentList.Where(s => (s.ClassCode == 1001) && (studentScodeList.Exists(p => (p.ScoreValue > 0 && p.ScoreValue < 90) && p.StudentCode == s.StudentCode)));
+            //    //foreach (Student stu in student1001) {
+            //    //    Console.WriteLine("学生姓名：{0}学生编号：{1}", stu.StudentName, stu.StudentCode);
+            //    //    //pr(stu);
+            //    //}
+            //    //foreach (Student stu in listOrderBy) {
+            //    //    Console.WriteLine("学生姓名：{0}学生编号：{1}", stu.StudentName, stu.StudentCode);
+            //    //    //pr(stu);
+            //    //}
+            //    //foreach (Student stu in listStartWith) {
+            //    //    Console.WriteLine("学生姓名：{0}学生编号：{1}", stu.StudentName, stu.StudentCode);
+            //    //    //pr(stu);
+            //    //}
+            //    PeopleType pt;
+            //    switch (pt)
+            //    {
+            //        case PeopleType.p1:
+            //            break;
+            //        case PeopleType.p2:
+            //            break;
+            //        case PeopleType.p3:
+            //            break;
+            //        case PeopleType.p4:
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //    foreach (Student sc in listScore)
+            //    {
+            //        Console.WriteLine("学生姓名：{0}学生编号：{1}", sc.StudentName, sc.StudentCode);
+            //        //pr(stu);
+            //    }
+            //}
+
+            TestEnumber.TestStreamReaderEnumerable();
+            Console.WriteLine("-------");
+            TestEnumber.TestReadingFile();
+
+            Console.Read();
+
+
+
+        }
+
+        // private static void show<T>(T t) {
+        //    PropertyInfo p = t.GetType().GetProperty("Student");
+        //    Console.WriteLine("学生姓名：{0}学生编号：{1}", (string)p.GetValue("StudentName"), (string)p.GetValue("StudentCode"));
+        //}
+
+        public enum PeopleType { 
+            p1,
+            p2,
+            p3,
+            p4
+        }
+    }
+   public class Student
+    {
+       public string StudentName { get; set; }
+       public int StudentCode { get; set; }
+       public int ClassCode { get; set; }
+       public string ClassName { get; set; }
+       public System.DateTime BirthDay { get; set; }
+       public int Grade { get; set; }
+       public Student(string studentName, int studentCode, int classCode, string className, System.DateTime birthDay) {
+            this.StudentName = studentName;
+            this.StudentCode = studentCode;
+            this.ClassCode = classCode;
+            this.ClassName = className;
+            this.BirthDay = birthDay;
+        }
+    }
+   public class Scode
+    {
+       public int StudentCode { get; set; }
+       public int ScoreValue { get; set; }
+       public string SubjectName { get; set; }
+       public int Grade { get; set; }
+        public Scode(int studentCode, int scoreValue, string subjectName) {
+            this.StudentCode = studentCode;
+            this.ScoreValue = scoreValue;
+            this.SubjectName = subjectName;
+        }
+    }
+
+
+}
---
+++ Grammar/TestEnumber.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.IO;
+
+namespace Grammar
+{
+    public class TestEnumber
+    {
+        public static void TestStreamReaderEnumerable() {
+            long memoryBefore = GC.GetTotalMemory(true);
+            IEnumerable<String> stringsFound;
+            try
+            {
+                stringsFound = from line in new StreamReaderEnumerable(@"F:\NET\temptempFile.txt")
+                               where line.Contains("string to search for")
+                               select line;
+                Console.WriteLine("Found:" + stringsFound.Count());
+            }
+            catch (FileNotFoundException) {
+                Console.WriteLine(@"This example requires a file named F:\NET\temptempFile.txt.");
+                return;
+            }
+
+            long memoryAfter = GC.GetTotalMemory(false);
+            Console.WriteLine("Memory Used With Iterator = \t"
+                                + string.Format(((memoryAfter - memoryBefore) / 1000).ToString(), "n") + "kb");
+        }
+
+        public static void TestReadingFile() {
+            long memoryBefore = GC.GetTotalMemory(true);
+            StreamReader sr;
+            try
+            {
+                sr = new StreamReader(@"F:\NET\temptempFile.txt");
+            }
+            catch (FileNotFoundException) {
+                Console.WriteLine(@"This example requires a file named F:\NET\temptempFile.txt.");
+                return;
+            }
+
+            List<string> fileContents = new List<string>();
+            while (!sr.EndOfStream) {
+                fileContents.Add(sr.ReadLine());
+            }
+
+            var stringsFound = from line in fileContents
+                               where line.Contains("string to search for")
+                               select line;
+
+            sr.Close();
+
+            Console.WriteLine("Found:" + stringsFound.Count());
+
+            long memoryAfter = GC.GetTotalMemory(false);
+            Console.WriteLine("Memory Used Without Iterator = \t"
+                                + string.Format(((memoryAfter - memoryBefore) / 1000).ToString(), "n") + "kb");
+        }
+
+    }
+    public class StreamReaderEnumerable : IEnumerable<string>
+    {
+        private string _filePath;
+        public StreamReaderEnumerable(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new StreamReaderEnumerator(_filePath);
+        }
+
+        private IEnumerator GetEnumerator1()
+        {
+            return this.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator1();
+        }
+    }
+
+    public class StreamReaderEnumerator : IEnumerator<string>
+    {
+        private StreamReader _sr;
+        public StreamReaderEnumerator(string filePath)
+        {
+            _sr = new StreamReader(filePath);
+        }
+        private string _current;
+
+        public string Current
+        {
+            get
+            {
+                if (_sr == null || _current == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                return _current;
+            }
+        }
+
+        private object Current1
+        {
+            get { return this.Current; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current1; }
+        }
+        //Implement MoveNext and Reset, which ara required by IEnumerator.
+        public bool MoveNext()
+        {
+            _current = _sr.ReadLine();
+            if (_current == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            _sr.DiscardBufferedData();
+            _sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            _current = null;
+        }
+        //Implement IDisposable, which is also implemented by IEnumerator(T).
+        private bool disposedValue = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    //Dispose of managed resoures.
+                }
+                _current = null;
+                if (_sr != null)
+                {
+                    _sr.Close();
+                    _sr.Dispose();
+                }
+            }
+            this.disposedValue = true;
+        }
+        ~StreamReaderEnumerator()
+        {
+            Dispose(false);
+        }
+    }
+}
