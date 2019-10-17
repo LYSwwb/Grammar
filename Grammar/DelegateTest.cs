50f274153442215d9789e0eaf78c923345bdc3e0
LYSwwb <mylocalmyhome@gmail.com>
2019-10-17 23:20:34 +0800

    添加委托代码

--- Grammar/DelegateTest.cs
+++ Grammar/DelegateTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Grammar
 {
@@ -16,6 +17,104 @@ namespace Grammar
      *     咱们来看一个实例，通过反编译工具咱们看一下，委托通过编译之后长什么样子
      *     在图中咱们是不是可以清楚的看到，委托真是一个类，它继承自System.Object；并且它还有自己的方法：Invoke();BeginInvoke();EndInvoke();
      *     那么要怎么使用呢？咱们通过一个实例来看一下：
+     *     
+     *     
+     *     
+     *   在图中咱们是不是可以清楚的看到，委托真的是一个类，继承自System.Object；并且它还有自己的方法：Invoke();BeginInvoke();EndInvoke();
+    *那么怎么使用委托呢？看下面的例子：
+    *        public delegate void ShowResult(int a,int b);
+    *        static void Main(string[] args)
+    *        {
+    *            ShowResult showResult = Add;
+    *            //ShowResult showResult = New ShowResult(Add);
+    *            DelegateTest.Calculate(showResult , 5, 6);
+    *            
+    *            
+    *            Console.Read();
+    *        }
+    *
+    *        public static void Add(int a, int b) {
+    *            Console.WriteLine(a + b);
+    *        }
+    *
+    *        public static void Divide(int a, int b) {
+    *            Console.WriteLine(a / b);
+    *        }
+    *        public static void Subtract(int a, int b) {
+    *            Console.WriteLine(a - b);
+    *        }
+    *        public static void Multiply(int a, int b) {
+    *            Console.WriteLine(a * b);
+    *        }
+    *
+    * 看了上面的例子是不是感觉委托没有什么用，我么直接调用相应的方法不就行了吗？为什么还要绕个远路呢？
+    * 我们来看一下上面的实例，实例的目的是输出两个数的加、减、乘、除，这四个方法目前有一个共性的操作就是需要计算的结果进行输出。这仅仅是目前的需求，但是为了不挖坑，对传进去的参数是不是需要检测。
+    * 现在针对现在的程序添加一些规则：1：a、b都要是正整数；2：对每次操作都要写入日志。
+    * 考虑一下，添加这些规则，咱们是不是需要怎么修改代码。
+    *         public delegate int ShowResult(int a,int b);
+    *         
+    *         static void Main(string[] args)
+    *         {
+    *             ShowResult showResult = Add;
+    *             //ShowResult showResult = New ShowResult(Add);
+    *             DelegateTest.Calculate(showResult , 5, 6);
+    *             Console.Read();
+    *         }
+    *         
+    *         
+    *         public static int Add(int a, int b) {
+    *             return a + b;
+    *         }
+    *         public static int Divide(int a, int b) {
+    *             return a / b;
+    *         }
+    *         public static int Subtract(int a, int b) {
+    *             return a - b;
+    *         }
+    *         public static int Multiply(int a, int b) {
+    *             return a * b;
+    *         }
+    * 
+    *         public static void Calculate(ShowResult showResult, int a, int b) {
+    *             if(a < 0 || b < 0){
+    *                 Console.WriteLine("参数有误");
+    *             }else{     
+    *                 System.Reflection.MethodInfo methodInfo = RuntimeReflectionExtensions.GetMethodInfo(showResult);
+    *                 Console.WriteLine("在这里写入一条日志" + methodInfo.ToString());
+    *                 Console.WriteLine(showResult(a, b));
+    *                 //Console.WriteLine(showResult?.Invoke(a, b)); ?.  null检查 C#6.0以上版本
+    *             }
+    *         }
+    * 
+    * 向上面提取出所有相同的代码，放到一个方法中，把不相同的部分，定义成响应的方法。不论以后需要怎么改需求只需要更改Calcute 方法就行了，这样写代码的灵活性是不是更高，可扩展性是不是有了提升。
+    * 协变：
+    * 
+    * 逆变：
+    * 
+    * 
+    * 下面介绍一下C#定义好的委托：
+    * 1、Action<T> delegate  封装一个方法，该方法没有返回值。其中可以有0~16个参数。
+    * pubilc delegate void Action<in T>(T obj); // T 泛型
+    * 
+    * 2、Func<T, TResult> delegate 封装一个带有0~16个参数，且返回TResult类型的方法
+    * public delegate TResult Func<out TResult>();  
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
+     *     
      *     
      * 
     */
@@ -36,7 +135,19 @@ namespace Grammar
         }
 
         public static void Calculate(ShowResult showResult, int a, int b) {
-            Console.WriteLine(showResult(a, b));
+            if (a < 0 || b < 0)
+            {
+                Console.WriteLine("参数有误");
+            }
+            else
+            {
+                System.Reflection.MethodInfo methodInfo = RuntimeReflectionExtensions.GetMethodInfo(showResult);
+                Console.WriteLine("在这里写入一条日志" + methodInfo.ToString());
+
+                Console.WriteLine(showResult?.Invoke(a, b));
+                //Console.WriteLine(showResult(a, b));
+            }
         }
+
     }
 }
--- Grammar/Program.cs
+++ Grammar/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,10 +19,45 @@ namespace Grammar
             //Console.WriteLine("-------");
             //TestEnumber.TestReadingFile();
 
-            DelegateTest.Calculate(DelegateTest.Add, 5, 6);
+            DelegateTest.ShowResult showResult = DelegateTest.Add;
+
+            DelegateTest.Calculate(showResult, 5, 6);
             DelegateTest.Calculate(DelegateTest.Divide, 5, 6);
             DelegateTest.Calculate(DelegateTest.Subtract, 5, 6);
             DelegateTest.Calculate(DelegateTest.Multiply, 5, 6);
+
+            //i*j+w*x
+            //ParameterExpression a = Expression.Parameter(typeof(int), "i");   //创建一个表达式树中的参数，作为一个节点，这里是最下层的节点
+            //ParameterExpression b = Expression.Parameter(typeof(int), "j");
+            //BinaryExpression r1 = Expression.Multiply(a, b);    //这里i*j,生成表达式树中的一个节点，比上面节点高一级
+
+            //ParameterExpression c = Expression.Parameter(typeof(int), "w");
+            //ParameterExpression d = Expression.Parameter(typeof(int), "x");
+            //BinaryExpression r2 = Expression.Multiply(c, d);
+
+            //BinaryExpression result = Expression.Add(r1, r2);   //运算两个中级节点，产生终结点
+
+            //Expression<Func<int, int, int, int, int>> lambda = Expression.Lambda<Func<int, int, int, int, int>>(result, a, b, c, d);
+
+            //Console.WriteLine(lambda + "");   //输出‘(i,j,w,x)=>((i*j)+(w*x))’，z对应参数b，p对应参数a
+
+            //Func<int, int, int, int, int> f = lambda.Compile();  //将表达式树描述的lambda表达式，编译为可执行代码，并生成该lambda表达式的委托；
+
+            //Console.WriteLine(f(1, 1, 1, 1) + "");  //输出结果2
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             Console.Read();
 
